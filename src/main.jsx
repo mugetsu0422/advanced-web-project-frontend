02c8033e3e4831b138ec12379c318dc2101528bd
Samuel <20127610@student.hcmus.edu.vn>
2023-11-09 23:00:27 +0700

    create navbar UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter, 
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
