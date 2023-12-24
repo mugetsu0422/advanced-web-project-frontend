99f50a8d45f3da14db1bbec6dd136e31c86d8e06
HuynhNguyen0811 <81243529+HuynhNguyen0811@users.noreply.github.com>
2023-12-24 10:01:32 +0700

    remove unnecessary function

--- src/teacher/TeacherGradeReviewDetail.jsx
+++ src/teacher/TeacherGradeReviewDetail.jsx
@@ -195,20 +195,6 @@ const TeacherGradeReviewDetail = () => {
       })
   }
 
-  const formatDateTime = (dateTimeString) => {
-    const date = new Date(dateTimeString)
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const day = date.getDate()
-    const month = date.getMonth() + 1
-    const year = date.getFullYear()
-
-    const formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`
-    const formattedDate = `${day}/${month}/${year}`
-
-    return `${formattedTime} ${formattedDate}`
-  }
-
   return (
     <div className={styles['detail-container']}>
       <Button
